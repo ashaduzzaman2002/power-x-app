66e4b13768e265c2c61386ca54ace3e03b365f36
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-07-29 01:36:52 +0530

    foward

--- src/screens/forward/Forward.jsx
+++ src/screens/forward/Forward.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./forward.css";
+import { rocket } from "../../assets";
 
 const Forward = () => {
-    const navigate = useNavigate()
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
     <div className="container">
       {/* Top Navbar */}
       <div class="app__top__nav">
         <div class="top__nav__cols">
-          <div class="top__nav__back__btn" onClick={() => navigate(location?.state?.from || "/")}>
+          <div
+            class="top__nav__back__btn"
+            onClick={() => navigate(location?.state?.from || "/")}
+          >
             <svg
               stroke="currentColor"
               fill="currentColor"
@@ -31,12 +36,114 @@ const Forward = () => {
         <div class="top__nav__cols"></div>
       </div>
 
+      <div>
+        <div className="transaction-history-card py-0">
+          <div className="info">
+            <i class="bi bi-person-circle" style={{ fontSize: "3rem", color: '#b3b1b1' }}></i>
+            <div>
+              <p className="m-0">Ashadu Zaman</p>
+              <p className="m-0">8023030340</p>
+            </div>
+          </div>
 
-      <div className='floating-btn d-flex align-items-center justify-content-center rounded-circle'>
-        <button className='w-100 h-100 d-flex justify-content-center align-items-center'><i class="bi bi-plus"></i></button>
-      </div>
-      </div>
-  )
-}
+          <div>
+          <i class="bi bi-trash3-fill text-light" style={{fontSize: '1.2rem'}}></i>
+          </div>
+        </div>
 
-export default Forward
+        <div className="transaction-history-card py-0">
+          <div className="info">
+            <i class="bi bi-person-circle" style={{ fontSize: "3rem", color: '#b3b1b1' }}></i>
+            <div>
+              <p className="m-0">Ashadu Zaman</p>
+              <p className="m-0">8023030340</p>
+            </div>
+          </div>
+
+          <div>
+          <i class="bi bi-trash3-fill text-light" style={{fontSize: '1.2rem'}}></i>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content forward-modal">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Add Friend
+              </h1>
+              <button
+                type="button"
+                className="modal-btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                style={{ color: "#fff" }}
+              >
+                <i class="bi bi-x-lg"></i>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div>
+                  <label for="exampleInputEmail1" class="form-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control text-light"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    style={{ background: "#d3d3d33b", border: "none" }}
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">
+                    Phone
+                  </label>
+                  <input
+                    type="number"
+                    class="form-control text-light"
+                    id="exampleInputPassword1"
+                    style={{ background: "#d3d3d33b", border: "none" }}
+                  />
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer gap-3">
+              <button type="button" class="close-btn" data-bs-dismiss="modal">
+                CLOSE
+              </button>
+              <button
+                type="button"
+                class="btn px-3 text-light"
+                style={{ background: "#fec007" }}
+              >
+                ADD
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="floating-btn d-flex align-items-center justify-content-center rounded-circle">
+        <button
+          type="button"
+          className="w-100 h-100 d-flex justify-content-center align-items-center"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          <i class="bi bi-plus"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Forward;
--- src/screens/games/FastParity.jsx
+++ src/screens/games/FastParity.jsx
@@ -9,36 +9,115 @@ const FastParity = () => {
   const firstCardList = ["A", "B", "C", "D"];
   const [activeBtn, setActiveBtn] = useState("probability");
   const [activeBtn2, setActiveBtn2] = useState("OtherPlayers");
+  const probabilityBox = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const location = useLocation();
 
   return (
     <div className="container">
-      <Header title={'Fast Parity'} />
+      <Header title={"Fast Parity"} />
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+        style={{ backdropFilter: "blur(2px)" }}
+      >
+        <div class="modal-dialog modal-dialog-centered">
+          <div className="modal-content start-box">
+            <h2 className="game-name">Gold</h2>
+            <p>Points</p>
+
+            <div className="points-div">
+              <h3>INR 0.0</h3>
+              <button onClick={() => navigate("/recharge")}>
+                <i className="fa-solid fa-clock-rotate-left"></i> Recharge
+              </button>
+            </div>
+
+            <div className="contract-point">
+              <p>Contract Points</p>
+
+              <div>
+                <button>10</button>
+                <button>100</button>
+                <button className={"contract-point-selected"}>1000</button>
+                <button>10000</button>
+              </div>
+            </div>
+
+            <div className="start-number-outer">
+              <p>Number</p>
+
+              <div className="start-number">
+                <div>
+                  <button>-5</button>
+                  <button>-1</button>
+                </div>
+                <p>1000</p>
+                <div>
+                  <button>+1</button>
+                  <button>+5</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="delivery">
+              <div>
+                <p>Delivery</p>
+                <p>50.00</p>
+              </div>
+
+              <div>
+                <p>Fee</p>
+                <p>0.00</p>
+              </div>
+
+              <div>
+                <p>Amount</p>
+                <p>+176.00</p>
+              </div>
+            </div>
+
+            <div className="w-100 mt-4">
+              <button
+                style={{
+                  backgroundColor: 'rgb(252, 148, 13)'
+                }}
+                className="btn w-100 text-light py-2"
+                data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+              >
+                Start
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div>
         <div className="wallet-container d-flex justify-content-between align-items-center gap-2 mt-3">
           <div className="parity-top flex-column align-items-center w-100 p-2 ">
-            <p>Win Wallet</p>
-            <div className="input-container d-flex mb-3 align-items-center gap-2 px-2 rounded">
-              <i class="bi bi-currency-rupee"></i>{" "}
-              <input className="p-1 w-100" type="text" placeholder="100" />
-            </div>
+            <p className="mb-1">Amount</p>
+            <p style={{ fontSize: "1.5rem", fontWeight: "500" }}>₹500.00</p>
 
             <button
               className="btn text-white rounded-pill w-100 fw-medium"
               style={{
                 backgroundColor: "#fc940d",
-                fontSize: 13
+                fontSize: 13,
               }}
             >
-             Withdraw
+              Withdraw
             </button>
 
             <button
               className="btn text-white rounded-pill w-100 fw-medium mt-2"
               style={{
                 backgroundColor: "#00c282",
-                fontSize: 13
+                fontSize: 13,
               }}
             >
               Transfer
@@ -46,19 +125,18 @@ const FastParity = () => {
           </div>
 
           <div className="parity-top flex-column align-items-center w-100 p-2">
-            <p>Play Wallet</p>
-            <div className="input-container d-flex mb-3 align-items-center gap-2 px-2 rounded">
-              <i class="bi bi-currency-rupee"></i>{" "}
-              <input className="p-1 w-100" type="text" placeholder="100" />
-            </div>
+            <p className="mb-1">Play Wallet</p>
+            <p style={{ fontSize: "1.5rem", fontWeight: "500" }}>₹500.00</p>
 
             <button
               className="btn text-white rounded-pill w-100 fw-medium"
               style={{
                 backgroundColor: "#fc940d",
-                fontSize: 13
+                fontSize: 13,
               }}
-              onClick={() => navigate('/recharge', { state: { from: location.pathname } })}
+              onClick={() =>
+                navigate("/recharge", { state: { from: location.pathname } })
+              }
             >
               Recharge
             </button>
@@ -67,9 +145,11 @@ const FastParity = () => {
               className="btn text-white rounded-pill w-100 fw-medium mt-2"
               style={{
                 backgroundColor: "#00c282",
-                fontSize: 13
+                fontSize: 13,
               }}
-              onClick={() => navigate('/forward', { state: { from: location.pathname } })}
+              onClick={() =>
+                navigate("/forward", { state: { from: location.pathname } })
+              }
             >
               Forward
             </button>
@@ -91,11 +171,11 @@ const FastParity = () => {
         </div>
 
         <div className="game-coins mt-4">
-          <div>
+          <div data-bs-toggle="modal" data-bs-target="#exampleModal">
             <img src={goldCoin} alt="" />
           </div>
 
-          <div>
+          <div data-bs-toggle="modal" data-bs-target="#exampleModal">
             <img
               src={silverCoin}
               alt=""
@@ -105,17 +185,30 @@ const FastParity = () => {
         </div>
 
         <div className="prity-colors">
-          <div className="p-3" style={{ backgroundColor: "#d72e2a" }}>
+          <div
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            className="p-3"
+            style={{ backgroundColor: "#d72e2a" }}
+          >
             <p className="m-0">Red</p>
             <p className="m-0 border-top w-75 text-center">2X</p>
           </div>
 
-          <div style={{ backgroundColor: "#388e3d" }}>
+          <div
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            style={{ backgroundColor: "#388e3d" }}
+          >
             <p className="m-0">green</p>
             <p className="m-0 border-top w-75 text-center">3X</p>
           </div>
 
-          <div style={{ backgroundColor: "#1976d3" }}>
+          <div
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            style={{ backgroundColor: "#1976d3" }}
+          >
             <p className="m-0">Blue</p>
             <p className="m-0 border-top w-75 text-center">4X</p>
           </div>
@@ -123,7 +216,7 @@ const FastParity = () => {
 
         <div className="paritynum-btns">
           {firstCardList.map((item, i) => (
-            <div key={i}>
+            <div data-bs-toggle="modal" data-bs-target="#exampleModal" key={i}>
               <p className="m-0">{item}</p>
               <p className="m-0 border-top w-50 text-center">{2 + i}X</p>
             </div>
@@ -163,13 +256,13 @@ const FastParity = () => {
           </button>
         </div>
 
-        {/* {activeBtn === 'continuos' && <ContinuousTab />}
+        {activeBtn === "continuos" && <ContinuousTab />}
 
-            {activeBtn === 'record' && <Record />} */}
+        {activeBtn === "record" && <Record />}
 
-        {/* {activeBtn === 'probability' && (
-              <Probability probabilityBox={probabilityBox} />
-            )} */}
+        {activeBtn === "probability" && (
+          <Probability probabilityBox={probabilityBox} />
+        )}
 
         <div className="gameDetails-btn-group">
           <button
@@ -254,5 +347,209 @@ const FastParity = () => {
     </div>
   );
 };
+
+function ContinuousTab({}) {
+  const fastParityContinuousList = [
+    0,
+    7,
+    "-",
+    8,
+    0,
+    6,
+    1,
+    9,
+    3,
+    5,
+    0,
+    7,
+    7,
+    8,
+    0,
+    7,
+    8,
+    0,
+    6,
+    1,
+    9,
+    3,
+  ];
+  return (
+    <div className="continuous-tab">
+      <div className="scroll-container">
+        {fastParityContinuousList?.map((item, i) => (
+          <div key={i} className="parity__records__circle">
+            <div className="parity__records__circle__no">{item}</div>
+            <div
+              className="parity__records__circle__inner"
+              style={{
+                backgroundColor:
+                  item === "-"
+                    ? "#fec007"
+                    : item % 2 === 0
+                    ? "#f44238"
+                    : "#3b8d3c",
+              }}
+            >
+              <div
+                className="parity__records__circle__col"
+                style={{
+                  background:
+                    item === 0 ? "#f24337" : item === 5 ? "#1f98ef" : "",
+                }}
+              ></div>
+              <div
+                className="parity__records__circle__col"
+                style={{
+                  background:
+                    item === 0 ? "#0f45a2" : item === 5 ? "#388e3d" : "",
+                }}
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const Record = ({}) => {
+  const numberList = [
+    2,
+    2,
+    2,
+    2,
+    9,
+    9,
+    0,
+    "-",
+    5,
+    9,
+    8,
+    8,
+    8,
+    8,
+    6,
+    4,
+    8,
+    7,
+    1,
+    4,
+    9,
+    7,
+    1,
+    2,
+    8,
+    1,
+    4,
+  ];
+
+  return (
+    <div className="parity-record">
+      <p>Fast Parity Record</p>
+      <div className="parity-record-box">
+        {numberList.map((item, i) => (
+          <div key={i} className="parity__records__circle">
+            <div className="parity__records__circle__no">{item}</div>
+            <div
+              className="parity__records__circle__inner"
+              style={{
+                backgroundColor:
+                  item === "-"
+                    ? "#fec007"
+                    : item % 2 === 0
+                    ? "#f44238"
+                    : "#3b8d3c",
+              }}
+            >
+              <div
+                className="parity__records__circle__col"
+                style={{
+                  background:
+                    item === 0 ? "#f24337" : item === 5 ? "#1f98ef" : "",
+                }}
+              ></div>
+              <div
+                className="parity__records__circle__col"
+                style={{
+                  background:
+                    item === 0 ? "#0f45a2" : item === 5 ? "#388e3d" : "",
+                }}
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Probability = ({ probabilityBox }) => (
+  <>
+    <div className="probability">
+      <p
+        style={{
+          textAlign: "center",
+          marginTop: "1.2rem",
+          fontSize: 15,
+        }}
+      >
+        1 item today
+      </p>
+    </div>
+
+    <div className="game-first-row">
+      <div>
+        <p>0</p>
+      </div>
+
+      <div>
+        <p>0</p>
+      </div>
+
+      <div>
+        <p>0</p>
+      </div>
+
+      <div className="game-first-row-box-out">
+        <p style={{ backgroundColor: "#d72e2a" }}>R</p>
+        <div
+          className="game-first-row-box"
+          style={{ backgroundColor: "#ffcdd2" }}
+        ></div>
+      </div>
+
+      <div className="game-first-row-box-out">
+        <p style={{ backgroundColor: "#1976d3" }}>B</p>
+        <div
+          className="game-first-row-box"
+          style={{ backgroundColor: "#bbdefa" }}
+        ></div>
+      </div>
+
+      <div className="game-first-row-box-out">
+        <p style={{ backgroundColor: "#388e3d" }}>G</p>
+        <div
+          className="game-first-row-box"
+          style={{ backgroundColor: "#c8e6ca" }}
+        ></div>
+      </div>
+    </div>
+
+    <div className="probabilty-game-second-row game-second-row">
+      {probabilityBox.map((item, i) => (
+        <div key={i} className="game-second-row-color">
+          <p>{item}</p>
+        </div>
+      ))}
+
+      {probabilityBox.map((item, i) => (
+        <div key={i} className="numbers">
+          <p>{item}</p>
+          <p>0</p>
+        </div>
+      ))}
+    </div>
+  </>
+);
 
 export default FastParity;
