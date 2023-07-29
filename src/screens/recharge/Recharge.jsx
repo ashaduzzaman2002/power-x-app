c84ea75cdbea80f2058c2abe48c514064a2df634
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-07-30 01:08:28 +0530

    updated

--- src/screens/recharge/Recharge.jsx
+++ src/screens/recharge/Recharge.jsx
@@ -31,7 +31,7 @@ const Recharge = () => {
         </p>
       </div>
 
-      <div className="mt-2 mb-5">
+      <div className="mt-2" style={{marginBottom: '100px'}}>
         <button
           onClick={() => navigate("/fast-parity")}
           className="w-100 mb-2 withdraw__btn"
